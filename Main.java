73e79f1ad9b8d20ff53bf2075a98f39ce8e1d983
Сашок <Anton@top.ru>
2024-04-11 21:42:33 +0300

    Исправить Main.java на нормальное описание вывода

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Text");
+        System.out.println("Тестовая строка");
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
